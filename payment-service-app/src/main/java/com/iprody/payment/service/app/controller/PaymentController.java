2f07fd450b2a1cf4987083f6e758e22d539733a1
lukan <lukandrii62@protonmail.com>
2026-01-31 20:22:54 +0200

    Lesson 5. Homework

--- payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
@@ -1,30 +1,30 @@
 package com.iprody.payment.service.app.controller;
 
-import com.iprody.payment.service.app.model.Payment;
+import com.iprody.payment.service.app.persistence.PaymentRepository;
+import com.iprody.payment.service.app.persistence.entity.Payment;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/payments")
+@RequiredArgsConstructor
 public class PaymentController {
 
-    private final Map<Long, Payment> payments = new HashMap<>();
+    private final PaymentRepository paymentRepository;
 
-    public PaymentController() {
-        payments.put(1L, new Payment(1L, 99.99));
-        payments.put(2L, new Payment(2L, 150.00));
-        payments.put(3L, new Payment(3L, 49.50));
-        payments.put(4L, new Payment(4L, 299.99));
-    }
-
-    @GetMapping("/{id}")
-    public Payment getPaymentById(@PathVariable Long id) {
-        return payments.get(id);
+    @GetMapping("/{guid}")
+    public Payment getPaymentById(@PathVariable UUID guid) {
+        return paymentRepository.findById(guid)
+                .orElseThrow(() ->
+                        new RuntimeException("Payment not found with guid: " + guid)
+                );
     }
 
     @GetMapping
     public List<Payment> getAllPayments() {
-        return new ArrayList<>(payments.values());
+        return paymentRepository.findAll();
     }
 }
