fa4e05f875c9881d54daf4eb31c27eab2f17fea8
lukan <lukandrii62@protonmail.com>
2026-02-11 09:07:09 +0200

    Lesson 7. Homework

---
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/persistence/PaymentFilter.java
@@ -0,0 +1,16 @@
+package com.iprody.payment.service.app.persistence;
+
+import com.iprody.payment.service.app.persistence.entity.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public record PaymentFilter (
+
+        String currency,
+        BigDecimal minAmount,
+        BigDecimal maxAmount,
+        Instant createdAfter,
+        Instant createdBefore,
+        PaymentStatus status
+) {}
--- payment-service-app/src/main/java/com/iprody/payment/service/app/persistence/PaymentRepository.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/persistence/PaymentRepository.java
@@ -3,11 +3,26 @@ package com.iprody.payment.service.app.persistence;
 import com.iprody.payment.service.app.persistence.entity.Payment;
 import com.iprody.payment.service.app.persistence.entity.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
-public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+public interface PaymentRepository extends JpaRepository<Payment, UUID>, JpaSpecificationExecutor<Payment> {
 
-    List<Payment> findByStatus(PaymentStatus status);
+    List<Payment> findByCurrencyAndStatus(String currency, PaymentStatus status);
+
+    @Query("SELECT p FROM Payment p WHERE p.currency = :currency AND p.amount >= :minAmount")
+    List<Payment> filter(String currency, BigDecimal minAmount);
+
+    @NativeQuery("""
+                    SELECT p
+                    FROM payments p
+                    WHERE (p.currency = :currency OR :currency is null)
+                      AND (p.amount >= :minAmount OR :minAmount is null)
+                    """)
+    List<Payment> filterNative(String currency, BigDecimal minAmount);
 }
