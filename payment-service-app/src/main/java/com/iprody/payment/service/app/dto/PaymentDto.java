177f8276116b973111d379ec3e93c65ddb1462ca
lukan <lukandrii62@protonmail.com>
2026-02-14 15:46:01 +0200

    Lesson 8. Homework

---
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/dto/PaymentDto.java
@@ -0,0 +1,34 @@
+package com.iprody.payment.service.app.dto;
+
+import com.iprody.payment.service.app.persistence.entity.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDto {
+
+    private UUID guid;
+
+    private UUID inquiryRefId;
+
+    private BigDecimal amount;
+
+    private String currency;
+
+    private UUID transactionRefId;
+
+    private PaymentStatus status;
+
+    private String note;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+}
--- payment-service-app/src/main/java/com/iprody/payment/service/app/service/PaymentService.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/service/PaymentService.java
@@ -1,35 +1,18 @@
 package com.iprody.payment.service.app.service;
 
+import com.iprody.payment.service.app.dto.PaymentDto;
 import com.iprody.payment.service.app.persistence.PaymentFilter;
-import com.iprody.payment.service.app.persistence.PaymentFilterFactory;
-import com.iprody.payment.service.app.persistence.PaymentRepository;
-import com.iprody.payment.service.app.persistence.entity.Payment;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
-@Service
-@RequiredArgsConstructor
-public class PaymentService {
+public interface PaymentService {
 
-    private final PaymentRepository paymentRepository;
+    PaymentDto get(UUID id);
 
-    public List<Payment> findAll() {
-        return this.paymentRepository.findAll();
-    }
+    Page<PaymentDto> search(PaymentFilter filter, Pageable pageable);
 
-    public Page<Payment> search(PaymentFilter filter, Pageable pageable) {
-        return this.paymentRepository.findAll(PaymentFilterFactory.fromFilter(filter), pageable);
-    }
-
-    public Page<Payment> searchPaged(PaymentFilter filter, Pageable
-            pageable) {
-        Specification<Payment> spec =
-                PaymentFilterFactory.fromFilter(filter);
-        return paymentRepository.findAll(spec, pageable);
-    }
+    List<PaymentDto> findAll();
 }
